de39d3d979ee1ef982eb28f8ec1a7bd53737e003
Chunyan Wang <chunyanwang@Chunyans-MacBook-Air.local>
2021-11-19 16:30:37 -0800

    final

--- spotifysong/plot.py
+++ spotifysong/plot.py
@@ -14,8 +14,8 @@ def plot_correlation():
     plot correlation's matrix to explore dependency between features 
     '''
     # init figure size
-    data = pd.read_csv('spotifysong/correlation.csv').drop(columns=['Unnamed: 0']).iloc[:11,:11]
-    #data = pd.read_csv('correlation.csv').drop(columns=['Unnamed: 0']).iloc[:11,:11]
+    data = pd.read_csv('spotifysong/correlation.csv').drop(columns=['Unnamed: 0'])
+    #data = pd.read_csv('correlation.csv').drop(columns=['Unnamed: 0'])
     rcParams['figure.figsize'] = 15, 20
     fig = plt.figure()
     sns.heatmap(data.corr(), annot=True, fmt=".2f")
